b363a53fffba76fbbdbe4a0545e4a9d124b9c028
haiyen2909 <nguyenngochaiyentkqn@gmail.com>
2017-09-17 20:00:57 +0700

    Mã nguồn của ngày đầu tiên
    tạo lớp contact và lớp program

---
+++ ContactsApp/Contact.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp
+{
+    class Contact
+    {
+        string ho;
+        string ten;
+        string diachi;
+        string sdt;
+
+        public Contact(string ho, string ten, string diachi, string sdt)
+        {
+            this.ho = ho;
+            this.ten = ten;
+            this.diachi = diachi;
+            this.sdt = sdt;
+        }
+
+        public void SetHo(string ho)
+        {
+            this.ho = ho;
+        }
+
+        public string GetHo()
+        {
+            return this.ho;
+        }
+
+        public void SetTen(string ten)
+        {
+            this.ten = ten;
+        }
+
+        public string GetTen()
+        {
+            return this.ten;
+        }
+
+        public void SetDiaChi(string diachi)
+        {
+            this.diachi = diachi;
+        }
+
+        public string GetDiaChi()
+        {
+            return this.diachi;
+        }
+
+        public void SetSDT(string sdt)
+        {
+            this.sdt = sdt;
+        }
+
+        public string GetSDT()
+        {
+            return this.sdt;
+        }
+    }
+}
